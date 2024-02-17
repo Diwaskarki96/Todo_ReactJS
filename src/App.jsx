be908bedb4d18fb636107a422de0a836f52e0057
Diwash Karki <diwaskarki96@gmail.com>
2024-02-18 00:08:40 +0545

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div className="h-screen w-full bg-black text-zinc-500 ">App</div>;
+  return (
+    <>
+      <NavBar />
+      <div className="container mx-auto ">
+        <div className="bg-black text-white">
+          <h1>Your Todos</h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav className="flex justify-between bg-[#F1F1F1] py-2">
+      <div className="logo">
+        <h1 className="font-bold text-xl"> Tasks</h1>
+      </div>
+      <ul className="flex gap-8 mx-9">
+        <li className="cursor-pointer hover:font-bold transition-all ">Home</li>
+        <li className="cursor-pointer hover:font-bold transition-all ">
+          Your Tasks
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
