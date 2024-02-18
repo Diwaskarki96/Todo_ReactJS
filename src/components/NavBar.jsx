8d1b533ee861077f41c501149fcce33d0ad17b98
Diwash Karki <diwaskarki96@gmail.com>
2024-02-18 23:41:02 +0545

    navbar minor changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <nav className="flex justify-between bg-[#F1F1F1] py-2">
-      <div className="logo">
+      <div className="logo cursor-pointer">
         <h1 className="font-bold text-xl"> Tasks</h1>
       </div>
       <ul className="flex gap-8 mx-9">
